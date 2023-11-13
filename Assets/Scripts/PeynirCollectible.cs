4e033f3139e269f1f6e987eadb830319085ec89f
Birdem58 <113117061+Birdem58@users.noreply.github.com>
2023-11-13 12:14:34 +0300

    düzeltme

--- Assets/Scripts/PeynirCollectible.cs
+++ Assets/Scripts/PeynirCollectible.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class PeynirCollectible : MonoBehaviour
 {   
     // Start is called before the first frame update
+
+    private MeshRenderer m_MeshRenderer;
+    private Collider m_Collider;
     void Start()
     {
-        
+        m_MeshRenderer = GetComponent<MeshRenderer>();  
+        m_Collider = GetComponent<Collider>();  
     }
 
     // Update is called once per frame
@@ -21,10 +25,19 @@ public class PeynirCollectible : MonoBehaviour
     {
         if(other.CompareTag("Player"))
         {
-            Vector3 newScale = transform.localScale + new Vector3(0.1f, 0.1f, 0.1f);
-            transform.localScale = newScale;
-
-            Destroy(gameObject);
+            Vector3 newScale =  new Vector3(0.1f, 0.1f, 0.1f);
+            other.transform.localScale += newScale;
+            StartCoroutine("OneSecondHide");
+            
         }
+    }
+
+    IEnumerator OneSecondHide()
+    {
+        m_MeshRenderer.enabled = false;
+        m_Collider.enabled = false;
+        yield return new WaitForSeconds(1);
+        m_MeshRenderer.enabled = true;
+        m_Collider.enabled = true;
     }
 }
