fdb5d3d2eaf196e2b1171e0e6b206702f34da1af
Birdem58 <113117061+Birdem58@users.noreply.github.com>
2023-11-13 13:15:43 +0300

    Merdivenlevelimantığıyapıldı

---
+++ Assets/Scripts/MerdivenCollider.cs
@@ -0,0 +1,105 @@
+using StarterAssets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MerdivenGirisCollider : MonoBehaviour
+{
+   // private Merdivenici merdivenici;
+    private float counter;
+    private float refreshTime = 0.5f;
+    private float temp;
+    private float oPos;
+    private float sPos;
+    private float jHeight;
+    private float stepOffset;
+    [SerializeField] private bool girdi = false;
+    [SerializeField] private ThirdPersonController thirdPersonController;
+    [SerializeField] private CharacterController characterController;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // merdivenici = GetComponent<Merdivenici>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // merdivenici.isOnMerdiven = girdi;
+        counter += Time.deltaTime;
+
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            thirdPersonController = other.GetComponent<ThirdPersonController>();
+            characterController = other.GetComponent<CharacterController>();
+            stepOffset = characterController.stepOffset;
+            jHeight = thirdPersonController.JumpHeight;
+            girdi = true;
+        }
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            thirdPersonController = other.GetComponent<ThirdPersonController>();
+            characterController = other.GetComponent<CharacterController>();
+            thirdPersonController.JumpHeight = 0;
+            sPos = other.transform.position.y;
+            if (counter >= refreshTime)
+            {
+                temp = sPos - oPos;
+                oPos = sPos;
+                counter = 0;
+                Debug.Log(temp);
+
+                if (temp >= 0.1f)
+                {
+                    Vector3 negativeScale = new Vector3(0.05f, 0.05f, 0.05f);
+                    Vector3 minimumScale = new Vector3(0.5f, 0.5f, 0.5f);
+
+
+
+
+                    if (other.transform.localScale.x > minimumScale.x && other.transform.localScale.y > minimumScale.y && other.transform.localScale.z > minimumScale.z)
+                    {
+                        other.transform.localScale -= negativeScale;
+                    }
+                    else
+                    {
+                        other.transform.localScale = minimumScale;
+                        characterController.stepOffset = 0.1f;
+                    }
+                }
+
+            }
+
+
+
+
+
+
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            thirdPersonController = other.GetComponent<ThirdPersonController>();
+            characterController = other.GetComponent<CharacterController>();
+            characterController.stepOffset = stepOffset;
+            thirdPersonController.JumpHeight = jHeight;
+            girdi = false;
+        }
+    }
+
+}
--- Assets/Scripts/PeynirCollectible.cs
+++ Assets/Scripts/PeynirCollectible.cs
@@ -3,28 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PeynirCollectible : MonoBehaviour
-{   
+{
     // Start is called before the first frame update
+
+    private MeshRenderer m_MeshRenderer;
+    private Collider m_Collider;
     void Start()
     {
-        
+        m_MeshRenderer = GetComponent<MeshRenderer>();
+        m_Collider = GetComponent<Collider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
-            Vector3 newScale = transform.localScale + new Vector3(0.1f, 0.1f, 0.1f);
-            transform.localScale = newScale;
+            Vector3 newScale = new Vector3(0.1f, 0.1f, 0.1f);
+            other.transform.localScale += newScale;
+            StartCoroutine("OneSecondHide");
 
-            Destroy(gameObject);
         }
+    }
+
+    IEnumerator OneSecondHide()
+    {
+        m_MeshRenderer.enabled = false;
+        m_Collider.enabled = false;
+        yield return new WaitForSeconds(1);
+        m_MeshRenderer.enabled = true;
+        m_Collider.enabled = true;
     }
 }
