f914fcbc414766086d7fd2a7f889f11d93450d3c
Birdem58 <113117061+Birdem58@users.noreply.github.com>
2023-11-29 13:38:14 +0300

    level0Danlevel1e geçiş yapıldı

--- Assets/Scripts/PeynirCollectible.cs
+++ Assets/Scripts/PeynirCollectible.cs
@@ -9,6 +9,8 @@ public class PeynirCollectible : MonoBehaviour
     private MeshRenderer m_MeshRenderer;
     private Collider m_Collider;
     public float maxScale = 2.5f;
+    public float artýsScale = 0.1f;
+    public bool isCollectible;
 
     void Start()
     {
@@ -30,17 +32,19 @@ public class PeynirCollectible : MonoBehaviour
             Vector3 maximumScale = new Vector3(maxScale, maxScale, maxScale);
             if (other.transform.localScale.x < maximumScale.x && other.transform.localScale.y < maximumScale.y && other.transform.localScale.z < maximumScale.z)
             {
-                Vector3 newScale = new Vector3(0.1f, 0.1f, 0.1f);
+                Vector3 newScale = new Vector3(artýsScale, artýsScale, artýsScale);
                 other.transform.localScale += newScale;
             }
             else
             {
                 other.transform.localScale = maximumScale;
             }
-           
-            StartCoroutine("OneSecondHide");
+            
+                StartCoroutine("OneSecondHide");
+            
 
-            }
+
+        }
         }
 
         IEnumerator OneSecondHide()
@@ -48,7 +52,11 @@ public class PeynirCollectible : MonoBehaviour
             m_MeshRenderer.enabled = false;
             m_Collider.enabled = false;
             yield return new WaitForSeconds(1);
+            if(isCollectible)
+            {
             m_MeshRenderer.enabled = true;
             m_Collider.enabled = true;
+            }
+            
         }
     } 
