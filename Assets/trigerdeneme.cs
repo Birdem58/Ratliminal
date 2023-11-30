485a24746dd57119e1679b5d85e57233bb6bcf24
Birdem58 <113117061+Birdem58@users.noreply.github.com>
2023-11-30 15:52:56 +0300

    level2RespawnDüzeltildi

--- Assets/trigerdeneme.cs
+++ Assets/trigerdeneme.cs
@@ -2,24 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RespawnTrigger : MonoBehaviour
+public class trigerdeneme : MonoBehaviour
 {
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
-    public Transform spawnPoint;
-    
-    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        other.transform.position = new Vector3(0f, 50f, 0f);
+        other.transform.position = new Vector3(0f,50f,0f);
+        Debug.Log("trigerred");
     }
 
     private void OnTriggerStay(Collider other)
     {
         other.transform.position = new Vector3(0f, 50f, 0f);
     }
+
     private void OnTriggerExit(Collider other)
     {
         other.transform.position = new Vector3(0f, 50f, 0f);
     }
-   
 }
