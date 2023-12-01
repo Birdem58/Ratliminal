068d1536b75f0c569f2af93c2ad53d1ef37d1678
Birdem58 <113117061+Birdem58@users.noreply.github.com>
2023-12-02 00:06:57 +0300

    Update Lvl1 Gecis

--- Assets/Scripts/lvl1eGecis.cs
+++ Assets/Scripts/lvl1eGecis.cs
@@ -5,15 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class lvl1eGecis : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
         SceneManager.LoadScene(2);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        SceneManager.LoadScene(2);
-    }
+    
 }
