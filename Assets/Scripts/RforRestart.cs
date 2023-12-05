cb381081a955bebad9090ecfd60f71fd2041658d
Birdem58 <113117061+Birdem58@users.noreply.github.com>
2023-12-05 10:29:44 +0300

    CinematicSahne Düzeltildi

--- Assets/Scripts/RforRestart.cs
+++ Assets/Scripts/RforRestart.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class lvl1eGecis : MonoBehaviour
+public class RforRestart : MonoBehaviour
 {
-
     // Start is called before the first frame update
-    void OnEnable()
+    void Start()
     {
-        SceneManager.LoadScene(2);
+        
     }
 
-    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
