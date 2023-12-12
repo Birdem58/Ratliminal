667906ec0d16fbef69275a0c5ca903aa3ca4dc65
Birdem58 <113117061+Birdem58@users.noreply.github.com>
2023-12-12 14:25:37 +0300

    Mirodaki değişilkikler yapıldı

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -23,7 +23,7 @@ public class Menu : MonoBehaviour
 
     public void BackButton()
     {
-
+        
     }
 
 
--- Assets/Scripts/MenuGecis.cs
+++ Assets/Scripts/MenuGecis.cs
@@ -9,5 +9,16 @@ public class MenuGecis : MonoBehaviour
     public void MenuyeGec()
     {
         SceneManager.LoadScene(0);
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            SceneManager.LoadScene(0);
+
+        }
     }
 }
